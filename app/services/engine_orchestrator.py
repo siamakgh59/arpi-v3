07d84a3387a97483d416cade9e170c1feb5115c4
siamakgh59 <77428575+siamakgh59@users.noreply.github.com>
2026-07-05 14:46:20 +0330

    feat: build Engine Orchestrator v2

--- app/services/engine_orchestrator.py
+++ app/services/engine_orchestrator.py
@@ -1,6 +1,33 @@
-from app.engines.collective.cie_engine import CollectiveIntelligenceEngine
+from app.engines.macro.macro_engine import MacroEngine
 
-cie_engine = CollectiveIntelligenceEngine()
-from app.data.providers.prediction_markets import PredictionMarketProvider
+from app.data.providers.prediction_markets import PredictionMarketsProvider
 from app.data.providers.market_data import MarketDataProvider
 from app.data.providers.macro_data import MacroDataProvider
+
+
+class EngineOrchestrator:
+
+    def __init__(self):
+        self.market_provider = MarketDataProvider()
+        self.macro_provider = MacroDataProvider()
+        self.prediction_provider = PredictionMarketsProvider()
+        self.macro_engine = MacroEngine()
+
+    def run(self):
+
+        market = self.market_provider.get_data()
+        macro = self.macro_provider.get_data()
+        prediction = self.prediction_provider.get_data()
+
+        data = {
+            "market": market,
+            "macro": macro,
+            "prediction": prediction
+        }
+
+        result = self.macro_engine.run(data)
+
+        return {
+            "status": "ok",
+            "macro": result
+        }
